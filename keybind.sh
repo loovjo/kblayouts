95d2d4d7489f743cd7a2e7d259d3e1a6df6d41f8
loovjo <jonathan.loov@gmail.com>
2022-07-14 13:21:07 +0200

    initial commit

---
+++ keybind.sh
@@ -0,0 +1,14 @@
+hidutil property --set '{"UserKeyMapping":
+[
+    {
+        "HIDKeyboardModifierMappingSrc":0x7000000e7,
+        "HIDKeyboardModifierMappingDst":0x7000000e1,
+    }, {
+        "HIDKeyboardModifierMappingSrc":0x7000000e5,
+        "HIDKeyboardModifierMappingDst":0x7000000e6,
+    }, {
+        "HIDKeyboardModifierMappingSrc":0x7000000e6,
+        "HIDKeyboardModifierMappingDst":0x7000000e3,
+    }
+]
+}' 1>/dev/null
